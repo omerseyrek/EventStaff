14c1d7cfd83e32481c021fc3e90af4e7c791451f
ömer seyrek <omerseyrek@hotmail.com>
2025-01-26 11:49:07 +0300

    use abstract class

--- Infra/MessageQue/IEventStafPublisher.cs
+++ Infra/MessageQue/IEventStafPublisher.cs
@@ -1,5 +1,6 @@
 using EventStaf.Models;
 using MassTransit;
+using System.Diagnostics;
 
 namespace EventStaf.Infra.MessageQue
 {
@@ -8,18 +9,63 @@ namespace EventStaf.Infra.MessageQue
 		Task PublishEventOperation(EventModel eventModel, string operationType);
 	}
 
-	public class EventStafPublisher : IEventStafPublisher
-	{
-		private readonly IPublishEndpoint _publishEndpoint;
 
-		public EventStafPublisher(IPublishEndpoint publishEndpoint)
+    public abstract class MessagePublisher
+    {
+        private readonly IPublishEndpoint _publishEndpoint;
+        protected MessagePublisher(IPublishEndpoint publishEndpoint)
+        {
+            _publishEndpoint = publishEndpoint;
+        }
+
+        private static readonly ActivitySource _activitySource = new("messaging.masstransit");
+
+        public async Task PublishMessage<T>(T message) where T : class
+        {
+            using var activity = _activitySource.StartActivity(
+                $"Publish:{typeof(T).Name}",
+                ActivityKind.Producer);
+
+            try
+            {
+                await _publishEndpoint.Publish(message, context =>
+                {
+                    if (activity != null)
+                    {
+                        // Explicitly add trace context
+                        context.Headers.Set("traceparent", activity.Id);
+
+                        // Add custom properties
+                        activity.SetTag("messaging.system", "rabbitmq");
+                        activity.SetTag("messaging.destination_kind", "queue");
+                        activity.SetTag("messaging.operation", "publish");
+                        activity.SetTag("messaging.message_type", typeof(T).Name);
+                    }
+                });
+
+                activity?.SetStatus(ActivityStatusCode.Ok);
+            }
+            catch (Exception ex)
+            {
+                activity?.SetStatus(ActivityStatusCode.Error, ex.Message);
+                throw ex;
+            }
+        }
+    }
+
+
+    public class EventStafPublisher : MessagePublisher, IEventStafPublisher
+	{
+		private readonly IPublishEndpoint _publishEndpoint2;
+
+		public EventStafPublisher(IPublishEndpoint publishEndpoint) : base(publishEndpoint)
 		{
-			_publishEndpoint = publishEndpoint;
+			_publishEndpoint2 = publishEndpoint;
 		}
 
 		public async Task PublishEventOperation(EventModel eventModel, string operationType)
 		{
-			await _publishEndpoint.Publish(new EventOperationMessage
+			await _publishEndpoint2.Publish(new EventOperationMessage
 			{
 				OperationId = Guid.NewGuid(),
 				OperationName = operationType,
